031a51a9a962f19e918c25eda0f875d6bbd3904b
Jojopose-Lyorix <joris.pergaix@gmail.com>
2026-01-29 14:02:25 +0100

    Initial commit

---
+++ src/pj_asterix/CL_Connection.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pj_asterix;
+
+/**
+ *
+ * @author pergaixj
+ */
+public class CL_Connection {
+    public static String url = "//falbala.futaie.org:3306/pergaixj_asterix";
+    public static String login ="pergaixj";
+    public static String password ="Fauthah0uara";
+}
+// https://github.com/Jojopose-Lyorix/pj_asterix.git
